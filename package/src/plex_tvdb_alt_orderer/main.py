19b2ba2d844838d01f7f031a08f658c83e074ff4
bpoxy <bejhan.jetha@gmail.com>
2023-06-09 14:57:15 -0600

    Leverage Click to simplify environment variable handling.

--- package/src/plex_tvdb_alt_orderer/main.py
+++ package/src/plex_tvdb_alt_orderer/main.py
@@ -1,6 +1,5 @@
 import click
 import inquirer
-import os
 import re
 import validators
 from plexapi.myplex import MyPlexAccount, PlexServer
@@ -11,21 +10,21 @@ from termcolor import colored
 from tvdb_v4_official import TVDB
 
 @click.command()
-@click.option("--plex-library", "plex_section_name", type=str, 
+@click.option("--plex-library", "plex_section_name", type=str, envvar="PLEX_LIBRARY",
               help="Your Plex TV show library name. Omit to use the PLEX_LIBRARY environment variable, choose from a list interactively or if your Plex server has a sole TV show library.")
-@click.option("--plex-password", type=str, 
+@click.option("--plex-password", type=str, envvar="PLEX_PASSWORD",
               help="Your Plex password. Omit to use the PLEX_PASSWORD environment variable or enter interactively.")
-@click.option("--plex-server", "plex_server_identifier", type=str, 
+@click.option("--plex-server", "plex_server_identifier", type=str, envvar="PLEX_SERVER",
               help="Your Plex server name (user/password authentication) or URL (token authentication). Omit to use the PLEX_SERVER environment variable or enter interactively.")
-@click.option("--plex-show", "plex_show_name", type=str, 
+@click.option("--plex-show", "plex_show_name", type=str, envvar="PLEX_SHOW",
               help="The name of the show in Plex. Omit to use the PLEX_SHOW environment variable or enter interactively.")
-@click.option("--plex-token", type=str, 
+@click.option("--plex-token", type=str, envvar="PLEX_TOKEN",
               help="Your Plex token. Omit to use the PLEX_TOKEN environment variable or enter interactively.")
-@click.option("--plex-user", type=str, 
+@click.option("--plex-user", type=str, envvar="PLEX_USER",
               help="Your Plex username. Omit to use the PLEX_USER environment variable or enter interactively.")
-@click.option("--tvdb-order", "tvdb_order_name", type=str, 
+@click.option("--tvdb-order", "tvdb_order_name", type=str, envvar="TVDB_ORDER",
               help="The TVDB order name (as specified for API-connected systems). Omit to use the TVDB_ORDER environment variable or choose from a list interactively.")
-@click.option("--tvdb-pin", type=str, 
+@click.option("--tvdb-pin", type=str, envvar="TVDB_PIN",
               help="Your TVDB subscriber PIN. Omit to use the TVDB_PIN environment variable or enter interactively.")
 
 def main(plex_section_name: str, plex_password: str, plex_server_identifier: str, plex_show_name: str, plex_token: str, plex_user: str, tvdb_order_name: str, tvdb_pin: str):
@@ -34,7 +33,7 @@ def main(plex_section_name: str, plex_password: str, plex_server_identifier: str
     plex_show = get_plex_show(plex_section, plex_show_name)
     plex_episodes = plex_show.episodes()
     
-    tvdb_pin = cli_env_prompt(tvdb_pin, "TVDB_PIN", "your TVDB subscriber PIN")
+    tvdb_pin = prompt_if_unspecified(tvdb_pin, "your TVDB subscriber PIN")
     tvdb = TVDB(apikey="5f119e31-f9c5-4f0c-b1c3-064b3225e7d9", pin=tvdb_pin)
     tvdb_id = next(match.group("id") for match in [re.match(r"tvdb://(?P<id>\d+)", guid.id) for guid in plex_show.guids] if match)
     tvdb_season_type = get_tvdb_season_type(tvdb, tvdb_id, tvdb_order_name)
@@ -42,15 +41,11 @@ def main(plex_section_name: str, plex_password: str, plex_server_identifier: str
     
     update_plex(plex_episodes, tvdb_episodes)
 
-def cli_env_prompt(cli: str, env: str, description: str):
-    return cli or os.getenv(env) or inquirer.prompt([inquirer.Text("answer", message=f"Enter {description}")])["answer"]
-
 def error_exit(text: str):
     print(colored(text, "red"))
     exit()
 
 def get_plex_section(plex_server: PlexServer, section_name: str) -> ShowSection:
-    section_name = section_name or os.getenv("PLEX_LIBRARY")
     sections = list(filter(lambda s: s.TYPE == "show", plex_server.library.sections()))
     sections_dict = {s.title: s for s in sections}
 
@@ -66,18 +61,18 @@ def get_plex_section(plex_server: PlexServer, section_name: str) -> ShowSection:
         return sections_dict[section_name]
 
 def get_plex_server(plex_password: str, plex_server_identifier: str, plex_token: str, plex_user: str) -> PlexServer:
-    plex_server_identifier = cli_env_prompt(plex_server_identifier, "PLEX_SERVER", "your Plex server name (user/password authentication) or URL (token authentication)")
+    plex_server_identifier = prompt_if_unspecified(plex_server_identifier, "your Plex server name (user/password authentication) or URL (token authentication)")
 
     if validators.url(plex_server_identifier):
-        return PlexServer(plex_server_identifier, cli_env_prompt(plex_token, "PLEX_TOKEN", "your Plex token"))
+        return PlexServer(plex_server_identifier, prompt_if_unspecified(plex_token, "your Plex token"))
 
-    plex_user = cli_env_prompt(plex_user, "PLEX_USER", "your Plex username")
-    plex_password = cli_env_prompt(plex_password, "PLEX_PASSWORD", "your Plex password")
+    plex_user = prompt_if_unspecified(plex_user, "your Plex username")
+    plex_password = prompt_if_unspecified(plex_password, "your Plex password")
     plex_account = MyPlexAccount(plex_user, plex_password)
     return plex_account.resource(plex_server_identifier).connect()
 
 def get_plex_show(section: ShowSection, show_name: str) -> Show:
-    show_name = cli_env_prompt(show_name, "PLEX_SHOW", "the name of the show")
+    show_name = prompt_if_unspecified(show_name, "the name of the show")
     shows = section.search(title=show_name)
 
     if len(shows) == 0:
@@ -90,7 +85,6 @@ def get_plex_show(section: ShowSection, show_name: str) -> Show:
         return shows_dict[show_name]
 
 def get_tvdb_season_type(tvdb: TVDB, tvdb_id: int, order_name: str) -> str:
-    order_name = order_name or os.getenv("TVDB_ORDER")
     season_types = tvdb.get_season_types(tvdb_id)
     season_types_dict = {s["name"]: s["type"] for s in season_types}
 
@@ -99,6 +93,9 @@ def get_tvdb_season_type(tvdb: TVDB, tvdb_id: int, order_name: str) -> str:
 
     order_name = order_name or inquirer.prompt([inquirer.List("order_name", message="Select the order to apply", choices=season_types_dict.keys())])["order_name"]
     return season_types_dict[order_name]
+
+def prompt_if_unspecified(value: str, description: str):
+    return value or inquirer.prompt([inquirer.Text("answer", message=f"Enter {description}")])["answer"]
 
 def update_plex(plex_episodes: list[Episode], tvdb_episodes: list[dict]):
     tvdb_episode_dict = {}
